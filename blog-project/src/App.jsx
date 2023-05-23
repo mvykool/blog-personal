4f005c621ce74bdc28bd9dce2cc091d26986f87a
maicol hernandez <maykolandres4@gmail.com>
2023-05-23 03:45:50 -0500

    created pages

--- blog-project/src/App.jsx
+++ blog-project/src/App.jsx
@@ -1,9 +1,70 @@
+import { 
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+  } from "react-router-dom"
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";  
+
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+// set layout properties
+
+
+const Layout = () => {
+  return (
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+
+// set router and links
+
+const router= createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/single",
+        element: <Single/>
+      },
+      {
+        path: "/write",
+        element:<Write/>
+      }
+    ]
+  },
+
+  {
+    path: "/register",
+    element: <Register/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  }
+])
+
+// set component
 
 function App() {
 
   return (
     <>
-<p>hello world</p>
+    <RouterProvider router={router} />
     </>
   )
 }
