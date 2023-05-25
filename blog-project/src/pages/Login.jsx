ded0d745af8add91c986c62c611ce175cd7244e4
maicol hernandez <maykolandres4@gmail.com>
2023-05-24 22:58:28 -0500

    created login and register

--- blog-project/src/pages/Login.jsx
+++ blog-project/src/pages/Login.jsx
@@ -1,13 +1,17 @@
-
+import { Link } from 'react-router-dom'; 
 
 const Login = () => {
   return (
 	<div className="auth">
     <h1>Login</h1>
       <form>
-        <input type="text" placeholder="user-name" />
-        <input type="text" placeholder="password" />
+        <input required type="text" placeholder="user-name" />
+        <input required type="text" placeholder="password" />
         <button>Login</button>
+
+        <p>this is an error</p>
+
+        <span>Dont you have an account?<Link to="/register">Register</Link> </span>
       </form>
   </div>
   )
--- blog-project/src/pages/Register.jsx
+++ blog-project/src/pages/Register.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Link } from 'react-router-dom'; 
 
 const Register = () => {
   return (
-	<div>Register</div>
+	<div className="auth">
+    <h1>Register</h1>
+      <form>
+        <input required type="text" placeholder="user-name" />
+        <input required type="text" placeholder="email" />
+        <input required type="text" placeholder="password" />
+        <button>Register</button>
+
+        <p>this is an error</p>
+
+        <span>Do you have an account?<Link to="/login">Login</Link> </span>
+      </form>
+  </div>
   )
 }
 
