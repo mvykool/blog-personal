c23e4960bc543c3365c730b1ff3f4dc69706682f
maicol hernandez <maykolandres4@gmail.com>
2023-05-24 23:19:38 -0500

    done with navbar

--- blog-project/src/components/Navbar.jsx
+++ blog-project/src/components/Navbar.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
-	<div>Navbar</div>
+	<div className="navbar">
+    <div className="container">
+      <h1 className="logo">Logo</h1>
+      <div className="links">
+        <Link className='link' to="/?cat=art">
+          <h6>ART</h6>
+        </Link>
+        <Link className='link' to="/?cat=science">
+          <h6>SCIENCE</h6>
+        </Link>
+        <Link className='link' to="/?cat=cinema">
+          <h6>CINEMA</h6>
+        </Link>
+        <Link className='link' to="/?cat=design">
+          <h6>DESIGN</h6>
+        </Link>
+        <Link className='link' to="/?cat=food">
+          <h6>FOOD</h6>
+        </Link>
+        <Link className='link' to="/?cat=techology">
+          <h6>TECHNOLOGY</h6>
+        </Link>
+        <span>John</span>
+        <span>Logout</span>
+        <span className='write'>
+          <Link className='link' to='/write'>Write</Link>
+        </span>
+      </div>
+    </div>
+  </div>
   )
 }
 
