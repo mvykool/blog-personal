71eb01c14bf13111434f590db612836a5789bc66
Maicol Hernandez <maykolandres4@gmail.com>
2025-04-14 03:49:28 -0400

    added tialwind

--- main/astro.config.mjs
+++ main/astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
 });
