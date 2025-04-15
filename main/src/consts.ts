6f5a594e50053a216faeb07dec57def3355f524e
Maicol Hernandez <maykolandres4@gmail.com>
2025-04-15 06:30:29 -0400

    removed light bg

--- main/src/consts.ts
+++ main/src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Fragments";
 export const SITE_DESCRIPTION = "Personal blog";
